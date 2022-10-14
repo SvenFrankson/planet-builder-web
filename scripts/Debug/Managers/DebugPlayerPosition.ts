0698a01f8fb72bb2eac7e9a4b3486d8eac89daae
SvenFrankson <sven.frankson@gmail.com>
2022-10-14 09:20:56 +0200

    Add Player coordinates to player position debug.

--- scripts/Debug/Managers/DebugPlayerPosition.ts
+++ scripts/Debug/Managers/DebugPlayerPosition.ts
@@ -7,6 +7,7 @@ class DebugPlayerPosition {
 
     public container: HTMLDivElement;
 
+    private _playerCoordinates: DebugDisplayTextValue;
     private _playerPosition: DebugDisplayVector3Value;
     private _playerLocalPosition: DebugDisplayVector3Value;
     private _playerSide: DebugDisplayTextValue;
@@ -25,6 +26,7 @@ class DebugPlayerPosition {
     public initialize(): void {
         this.container = document.querySelector("#debug-player-position");
         
+        this._playerCoordinates = document.querySelector("#player-coordinates") as DebugDisplayTextValue;
         this._playerPosition = document.querySelector("#player-position") as DebugDisplayVector3Value;
         this._playerLocalPosition = document.querySelector("#player-local-position") as DebugDisplayVector3Value;
         this._playerSide = document.querySelector("#player-planet-side") as DebugDisplayTextValue;
@@ -37,11 +39,31 @@ class DebugPlayerPosition {
 
     private _update = () => {
         let position = this.game.player.position.clone();
+        let longitude = - VMath.AngleFromToAround(BABYLON.Axis.Z, position, BABYLON.Axis.Y) / Math.PI * 180;
+        let latitude = 0;
+        let heading = 0;
+        if (position.y * position.y === position.lengthSquared()) {
+            latitude = Math.sign(position.y) * 90;
+        }
+        else {
+            let equatorPosition = position.clone();
+            equatorPosition.y = 0;
+            let axis = BABYLON.Vector3.Cross(position, BABYLON.Axis.Y);
+            if (axis.lengthSquared() > 0) {
+                latitude = VMath.AngleFromToAround(equatorPosition, position, axis) / Math.PI * 180;
+            }
+
+            let northPole = new BABYLON.Vector3(0, this.game.player.planet.kPosMax * PlanetTools.CHUNCKSIZE, 0);
+            let northDir = northPole.subtract(position);
+            let dir = this.game.player.forward;
+            heading = VMath.AngleFromToAround(northDir, dir, position) / Math.PI * 180;
+        }
+        this._playerCoordinates.setText("Lat " + latitude.toFixed(0) + "° Lon " + longitude.toFixed(0) + "° Hdg " + heading.toFixed(0) + "°");
+
         this._playerPosition.setValue(position);
 
         let planetSide = PlanetTools.WorldPositionToPlanetSide(this.game.player.planet, position);
 
-        let invert: BABYLON.Matrix = planetSide.computeWorldMatrix(true).clone().invert();
         let quat = planetSide.rotationQuaternion.clone();
         let localPos: BABYLON.Vector3 = position.clone()
         position.rotateByQuaternionToRef(quat, localPos);
--- scripts/app.js
+++ scripts/app.js
@@ -970,9 +970,27 @@ class DebugPlayerPosition {
         this._initialized = false;
         this._update = () => {
             let position = this.game.player.position.clone();
+            let longitude = -VMath.AngleFromToAround(BABYLON.Axis.Z, position, BABYLON.Axis.Y) / Math.PI * 180;
+            let latitude = 0;
+            let heading = 0;
+            if (position.y * position.y === position.lengthSquared()) {
+                latitude = Math.sign(position.y) * 90;
+            }
+            else {
+                let equatorPosition = position.clone();
+                equatorPosition.y = 0;
+                let axis = BABYLON.Vector3.Cross(position, BABYLON.Axis.Y);
+                if (axis.lengthSquared() > 0) {
+                    latitude = VMath.AngleFromToAround(equatorPosition, position, axis) / Math.PI * 180;
+                }
+                let northPole = new BABYLON.Vector3(0, this.game.player.planet.kPosMax * PlanetTools.CHUNCKSIZE, 0);
+                let northDir = northPole.subtract(position);
+                let dir = this.game.player.forward;
+                heading = VMath.AngleFromToAround(northDir, dir, position) / Math.PI * 180;
+            }
+            this._playerCoordinates.setText("Lat " + latitude.toFixed(0) + "° Lon " + longitude.toFixed(0) + "° Hdg " + heading.toFixed(0) + "°");
             this._playerPosition.setValue(position);
             let planetSide = PlanetTools.WorldPositionToPlanetSide(this.game.player.planet, position);
-            let invert = planetSide.computeWorldMatrix(true).clone().invert();
             let quat = planetSide.rotationQuaternion.clone();
             let localPos = position.clone();
             position.rotateByQuaternionToRef(quat, localPos);
@@ -994,6 +1012,7 @@ class DebugPlayerPosition {
     }
     initialize() {
         this.container = document.querySelector("#debug-player-position");
+        this._playerCoordinates = document.querySelector("#player-coordinates");
         this._playerPosition = document.querySelector("#player-position");
         this._playerLocalPosition = document.querySelector("#player-local-position");
         this._playerSide = document.querySelector("#player-planet-side");
