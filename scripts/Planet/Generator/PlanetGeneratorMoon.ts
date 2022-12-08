505d7460b904584cabafbd11bb4cc5f2cbac88cd
SvenFrankson <sven.frankson@gmail.com>
2022-12-08 22:00:52 +0100

    Add craters on GeneratorMoon

--- scripts/Planet/Generator/PlanetGeneratorMoon.ts
+++ scripts/Planet/Generator/PlanetGeneratorMoon.ts
@@ -1,9 +1,18 @@
 class PlanetGeneratorFlat extends PlanetGenerator {
 
+    private _craterMap: PlanetHeightMap;
+
     constructor(planet: Planet) {
         super(planet);
 
         this.altitudeMap = PlanetHeightMap.CreateConstantMap(planet.degree, this.planet.seaLevelRatio);
+
+        this._craterMap = PlanetHeightMap.CreateConstantMap(planet.degree, 0);
+        for (let i = 0; i < 200; i++) {
+            this._craterMap.setRandomDisc(1, 2, 4);
+        }
+        this._craterMap.smooth();
+        this._craterMap.smooth();
     }
     
 
@@ -22,16 +31,16 @@ class PlanetGeneratorFlat extends PlanetGenerator {
     }
 
     public makeData(chunck: PlanetChunck, refData: number[][][], refProcedural: ProceduralTree[]): void {
+        let f = Math.pow(2, this._craterMap.degree - chunck.degree);
 
         for (let i: number = 0; i < PlanetTools.CHUNCKSIZE; i++) {
             refData[i - chunck.firstI] = [];
             for (let j: number = 0; j < PlanetTools.CHUNCKSIZE; j++) {
+                let craterDepth = this._craterMap.getForSide(chunck.side, (chunck.iPos * PlanetTools.CHUNCKSIZE + i) * f, (chunck.jPos * PlanetTools.CHUNCKSIZE + j) * f) * 3;
+
                 refData[i - chunck.firstI][j - chunck.firstJ] = [];
 
-                let di = (i - PlanetTools.CHUNCKSIZE * 0.5) / (PlanetTools.CHUNCKSIZE * 0.5);
-                let dj = (j - PlanetTools.CHUNCKSIZE * 0.5) / (PlanetTools.CHUNCKSIZE * 0.5);
-                let s = 3 * (1 - Math.floor(di * di + dj * dj));
-                let altitude = this.planet.seaLevel + Math.floor(s * Math.random());
+                let altitude = this.planet.seaLevel - craterDepth;
                 
                 for (let k: number = 0; k < PlanetTools.CHUNCKSIZE; k++) {
                     let globalK = k + chunck.kPos * PlanetTools.CHUNCKSIZE;
