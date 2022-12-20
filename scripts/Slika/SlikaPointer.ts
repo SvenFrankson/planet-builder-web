c731b77f0ff4384702b0e0d34c9408cf10852806
SvenFrankson <sven.frankson@gmail.com>
2022-12-20 01:32:24 +0100

    Update SlikaPointer style.

--- scripts/Slika/SlikaPointer.ts
+++ scripts/Slika/SlikaPointer.ts
@@ -1,77 +1,104 @@
+interface ISlikaPointerProperties {
+    x: number,
+    y: number,
+    xMin?: number,
+    yMin?: number,
+    xMax?: number,
+    yMax?: number,
+    radius?: number,
+    color?: BABYLON.Color3
+}
+
+function DefaultSlikaPointerProperties(prop: ISlikaPointerProperties): void {
+    if (isNaN(prop.xMin)) {
+        prop.xMin = 1;
+    }
+    if (isNaN(prop.yMin)) {
+        prop.yMin = 1;
+    }
+    if (isNaN(prop.xMax)) {
+        prop.xMax = 1;
+    }
+    if (isNaN(prop.yMax)) {
+        prop.yMax = 1;
+    }
+    if (isNaN(prop.radius)) {
+        prop.radius = 1;
+    }
+    if (!prop.color) {
+        prop.color = BABYLON.Color3.White();
+    }
+}
+
 class SlikaPointer extends SlikaElement {
 
-    private _lines: SlikaLine[] = [];
-    private _circle: SlikaCircle;
-    private _width: number = 4;
+    protected _lines: SlikaLine[] = [];
+    protected _circle: SlikaCircle;
+    protected _width: number = 4;
+    public XToDY: (x: number) => number;
+    public YToDX: (y: number) => number;
 
-    constructor(
-        public position: SPosition,
-        public min: SPosition,
-        public max: SPosition,
-        public r: number,
-        public color: BABYLON.Color3 = BABYLON.Color3.White(),
-        public XToDY?: (x: number) => number,
-        public YToDX?: (y: number) => number
-    ) {
+    constructor(public prop: ISlikaPointerProperties) {
         super();
+        DefaultSlikaPointerProperties(prop);
 
-        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: color, alpha: 1, width: this._width, outlineWidth: 2 }));
-        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: color, alpha: 1, width: this._width, outlineWidth: 2 }));
-        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: color, alpha: 1, width: this._width, outlineWidth: 2 }));
-        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: color, alpha: 1, width: this._width, outlineWidth: 2 }));
+        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 }));
+        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 }));
+        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 }));
+        this._lines.push(new SlikaLine({ x0: 0, y0: 0, x1: 0, y1: 0, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 }));
         
-        this._circle = new SlikaCircle({ x: 0, y: 0, r: this.r, color: color, alpha: 1, width: this._width, outlineWidth: 2 });
+        this._circle = new SlikaCircle({ x: 0, y: 0, r: this.prop.radius, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 });
     }
 
     public setPosition(x: number, y: number): void {
-        this.position.x = x;
-        this.position.x = Math.max(this.min.x + this.r, Math.min(this.max.x - this.r, this.position.x));
-        this.position.y = y;
-        this.position.y = Math.max(this.min.y + this.r, Math.min(this.max.y - this.r, this.position.y));
+        this.prop.x = x;
+        this.prop.x = Math.max(this.prop.xMin + this.prop.radius, Math.min(this.prop.xMax - this.prop.radius, this.prop.x));
+        this.prop.y = y;
+        this.prop.y = Math.max(this.prop.xMin + this.prop.radius, Math.min(this.prop.yMax - this.prop.radius, this.prop.y));
 
         this._updatePosition();
     }
 
-    private _updatePosition(): void {
+    protected _updatePosition(): void {
         let dx = 0;
         if (this.YToDX) {
-            dx = this.YToDX(this.position.y);
+            dx = this.YToDX(this.prop.y);
         }
         let dy = 0;
         if (this.XToDY) {
-            dy = this.XToDY(this.position.x);
+            dy = this.XToDY(this.prop.x);
         }
 
-        this._lines[0].prop.x0 = this.min.x + dx;
-        this._lines[0].prop.y0 = this.position.y;
-        this._lines[0].prop.x1 = this.position.x - this.r - 3 * this._width;
-        this._lines[0].prop.y1 = this.position.y;
+        this._lines[0].prop.x0 = this.prop.xMin + dx;
+        this._lines[0].prop.y0 = this.prop.y;
+        this._lines[0].prop.x1 = this.prop.x - this.prop.radius - 3 * this._width;
+        this._lines[0].prop.y1 = this.prop.y;
 
         this._lines[0].display = this._lines[0].prop.x0 < this._lines[0].prop.x1;
         
-        this._lines[1].prop.x0 = this.position.x;
-        this._lines[1].prop.y0 = this.min.y + dy;
-        this._lines[1].prop.x1 = this.position.x;
-        this._lines[1].prop.y1 = this.position.y - this.r - 3 * this._width;
+        this._lines[1].prop.x0 = this.prop.x;
+        this._lines[1].prop.y0 = this.prop.xMin + dy;
+        this._lines[1].prop.x1 = this.prop.x;
+        this._lines[1].prop.y1 = this.prop.y - this.prop.radius - 3 * this._width;
         
         this._lines[1].display = this._lines[1].prop.y0 < this._lines[1].prop.y1;
         
-        this._lines[2].prop.x0 = this.position.x + this.r + 3 * this._width;
-        this._lines[2].prop.y0 = this.position.y;
-        this._lines[2].prop.x1 = this.max.x - dx;
-        this._lines[2].prop.y1 = this.position.y;
+        this._lines[2].prop.x0 = this.prop.x + this.prop.radius + 3 * this._width;
+        this._lines[2].prop.y0 = this.prop.y;
+        this._lines[2].prop.x1 = this.prop.xMax - dx;
+        this._lines[2].prop.y1 = this.prop.y;
         
         this._lines[2].display = this._lines[2].prop.x0 < this._lines[2].prop.x1;
         
-        this._lines[3].prop.x0 = this.position.x;
-        this._lines[3].prop.y0 = this.position.y + this.r + 3 * this._width;
-        this._lines[3].prop.x1 = this.position.x;
-        this._lines[3].prop.y1 = this.max.y - dy;
+        this._lines[3].prop.x0 = this.prop.x;
+        this._lines[3].prop.y0 = this.prop.y + this.prop.radius + 3 * this._width;
+        this._lines[3].prop.x1 = this.prop.x;
+        this._lines[3].prop.y1 = this.prop.yMax - dy;
         
         this._lines[3].display = this._lines[3].prop.y0 < this._lines[3].prop.y1;
 
-        this._circle.prop.x = this.position.x;
-        this._circle.prop.y = this.position.y;
+        this._circle.prop.x = this.prop.x;
+        this._circle.prop.y = this.prop.y;
 
         if (this.slika) {
             this.slika.needRedraw = true;
@@ -84,4 +111,36 @@ class SlikaPointer extends SlikaElement {
         });
         this._circle.redraw(context);
     }
+}
+
+class SlikaPointerLite extends SlikaPointer {
+
+    constructor(public prop: ISlikaPointerProperties) {
+        super(prop);
+        
+        this._circle = new SlikaCircle({ x: 0, y: 0, r: this.prop.radius, color: this.prop.color, alpha: 1, width: this._width, outlineWidth: 2 });
+
+        this.animateRadius = AnimationFactory.CreateNumber(this, this._circle.prop, "r",
+            () => {
+                if (this.slika) {
+                    this.slika.needRedraw = true;
+                }
+            }
+        );
+    }
+
+    protected _updatePosition(): void {
+        this._circle.prop.x = this.prop.x;
+        this._circle.prop.y = this.prop.y;
+
+        if (this.slika) {
+            this.slika.needRedraw = true;
+        }
+    }
+
+    public redraw(context: BABYLON.ICanvasRenderingContext): void {
+        this._circle.redraw(context);
+    }
+
+    public animateRadius = AnimationFactory.EmptyNumberCallback;
 }
