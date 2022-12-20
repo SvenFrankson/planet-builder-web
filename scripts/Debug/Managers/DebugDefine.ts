98bd8bc13580a90bd564c0b18e50f58a69e5303d
SvenFrankson <sven.frankson@gmail.com>
2022-12-20 01:32:52 +0100

    Add PlayerArmTarget debug.

--- scripts/Debug/Managers/DebugDefine.ts
+++ scripts/Debug/Managers/DebugDefine.ts
@@ -4,6 +4,7 @@ class DebugDefine {
 
     public static USE_CHUNCK_LEVEL_DEBUG_COLORS: boolean = false;
 
+    public static SHOW_PLAYER_ARM_CURRENT_TARGET: boolean = true;
     public static SHOW_PLAYER_COLLISION_MESHES: boolean = false;
 
     public static LOG_GLOBAL_START_TIME_PERFORMANCE: boolean = false;
